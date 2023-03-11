48710f7112a2828d1308475c489acbfb6b6f7e4a
balajeerjs <rjsb0804@gmail.com>
2023-03-11 20:28:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import datetime
+import twitterscrap
+import pandas as pd
+
+
+#Initializing Variables
+diplay_error = False
+info_display = False
+message = ""
+
+#Setting session state to store results
+if 'tweets_list2' not in st.session_state:
+    st.session_state['tweets_list2'] = []
+
+
+#Converting list to dataframe
+
+
+
+def scrap():
+   final_query=(query_text
+   +" "
+   +"since:"
+   +str(from_date)
+   +" "
+   +"until:"+str(to_date))
+   tweets_list2=twitterscrap.scrap(final_query,limit)
+   return tweets_list2
+
+#Uploading to Database
+def upload(tweets_list2):
+   
+        message=twitterscrap.insert_data(query_text,tweets_list2)
+        return message
+   
+       
+@st.cache_data
+def convert_to_csv(df):
+    return df.to_csv().encode('utf-8')
+@st.cache_data
+def convert_to_json(df):
+    
+    return df.to_json( orient = 'records').encode('utf-8')
+
+#Comfiguring Streamlit GUI
+st.set_page_config(page_title='Twitter Scrapper',page_icon=':hash:',layout='wide')
+
+
+with st.container():
+    st.title("Welcome to Twitter Scrap")
+    buff, col, buff2 = st.columns([1,1,1])
+    with buff:
+      st.image('Title.jpg',output_format="JPEG")
+    with col:
+      st.subheader("Please Enter below details")
+      query_text = st.text_input(label='Enter Keyword or Hashtag',
+                                 placeholder="Enter Keyword or Hashtag")
+      
+      from_date=st.date_input("From Date",datetime.date(2023, 1, 1))
+
+      to_date=st.date_input("To Date",datetime.date(2023, 1,2))
+
+      limit=st.text_input(label='Enter number of tweets',
+                          placeholder="Enter number of tweets")
+      
+      col1, col2,col3 = st.columns([1,2,1])
+      with col1:
+       if st.button('SUBMIT'):
+          if query_text !="" and limit!="":
+           st.session_state['tweets_list2']=scrap()
+          else:
+             diplay_error=True
+      
+      with col2:   
+       if len(st.session_state['tweets_list2'])!=0:
+         if st.button('UPLOAD TO DATABASE'):
+          
+          message=upload(st.session_state['tweets_list2'])
+          info_display=True
+      with st.container():
+       if len(st.session_state['tweets_list2'])!=0:
+         file_type = st.radio("Choose File type for download",
+                               ('CSV', 'JSON'))
+         df = twitterscrap.convert_to_df(st.session_state['tweets_list2'])
+         if file_type=='CSV':
+            data=convert_to_csv(df)
+            st.download_button( label="Download data as CSV",
+                               data=data,file_name='Twitter_scrap.csv',
+                               mime='text/csv')
+         else:
+            data=convert_to_json(df)
+            st.download_button( label="Download data as JSON",
+                               data=data,file_name='Twitter_scrap.json',
+                               mime='text/json')
+         
+          
+      with st.container():
+        if diplay_error:
+                     
+          if query_text =="" and limit=="":
+              st.error('Please enter Keyword/Hashtag and number of tweets')
+          elif query_text=="":
+              st.error('Please enter Keyword/Hashtag')
+          elif limit=="":
+              st.error('Please enter number of tweets')
+          else:
+              pass
+        elif info_display:
+          if message=='Successfully uploaded':
+              st.success(message)
+          else:
+             st.error(message)
+                                  
+with st.container():
+    st.subheader("Displaying the results")
+    if len(st.session_state['tweets_list2'])!=0:
+        tweets_df2=twitterscrap.convert_to_df(st.session_state['tweets_list2'])
+        st.dataframe(tweets_df2)
+    else:
+        st.markdown("<b>NO RESULTS FOUND</b>",unsafe_allow_html=True)
+        
+    
+      
+      
---
+++ twitterscrap.py
@@ -0,0 +1,71 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from datetime import date
+import json
+import pprint
+import certifi
+import streamlit as st
+
+#Connecting to the database
+username=st.secrets['Mongo_username']
+password=st.secrets['Mongo_pwd']
+@st.cache_resource
+def connect_db(username,password):
+    print("I am running")
+    try:
+      con=MongoClient("mongodb+srv://"+username+":"+password+"@cluster0.2lvjncx.mongodb.net/?retryWrites=true&w=majority",
+                      tlsCAFile=certifi.where())
+      return con
+    except ConnectionFailure:
+        return 'Fail'
+    except:
+        return 'Fail'
+    
+@st.cache_data
+def convert_to_df(input_data):
+   tweets_df2 = pd.DataFrame(input_data,
+                             columns=['Date', 'Tweet Id','URL','UserName', 'Content', 'ReplyCount',
+                            'RetweetCount','Language','Source','LikeCount'])
+   
+   return tweets_df2
+  
+#Scrapping the data from twiiter
+def scrap(final_query,limit):
+    limit=int(limit)
+    tweets_list2 = []
+   
+#Using TwitterSearchScraper to scrape data and append tweets to list
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(final_query).get_items()):
+        if i>limit:
+            break
+        tweets_list2.append([ tweet.date, tweet.id,tweet.url,tweet.user.username,
+                              tweet.content,tweet.replyCount,tweet.retweetCount,
+                              tweet.lang,tweet.source,tweet.likeCount])
+
+    tweets_df2 = convert_to_df(tweets_list2)
+    #tweets_df2.to_json('file.json', orient = 'records', compression = 'infer', index = 'true')
+    return tweets_list2
+
+def insert_data(query_text,tweets_list2):
+    if len(tweets_list2) !=0 : 
+        
+        client = connect_db(username,password)
+        if client!='Fail':
+            db = client.twitterscrap
+            document=db.scrap_data
+            tweets_df2 = convert_to_df(tweets_list2)
+            result = tweets_df2.to_json(orient="records")
+            parsed = json.loads(result)
+            pprint.pprint(parsed)
+            currentdate=str(date.today())
+            mystore= {'Scraped Word': query_text, 'Scraped Date':currentdate ,'Scraped Data' :parsed}
+            x = document.insert_one(mystore)
+            print(x.inserted_id)
+            return 'Successfully uploaded'
+        else:
+            return 'Sorry,Not able to connect to Database.'
+    else :
+        return 'No tweets are found'
+
